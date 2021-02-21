75776e2b0732165f4e141e18cdae0fe8fec99b5e
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-02-21 13:56:15 +0000

    made the prompts work and installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
+// const util = require('util');
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -78,10 +78,55 @@ const questions = [{
 }, ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// function generateREADME(answers) {
+//     return `# ${questions.title}
+//     - ${questions.description}
+
+//     ## Link
+//     Launch the app ---> [here](${questions.repository})
+
+//     ## User Story
+
+//     - AS A traveler
+//     - I WANT to see the weather outlook for multiple cities
+//     - SO THAT I can plan a trip accordingly
+
+
+//     ## Acceptance Criteria
+//     - GIVEN a weather dashboard with form inputs
+//     - WHEN I search for a city
+//     - THEN I am presented with current and future conditions for that city and that city is added to the search history
+//     - WHEN I view current weather conditions for that city
+//     - THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//     - WHEN I view the UV index
+//     - THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+//     - WHEN I view future weather conditions for that city
+//     - THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+//     - WHEN I click on a city in the search history
+//     - THEN I am again presented with current and future conditions for that city
+//     - WHEN I open the weather dashboard
+//     - THEN I am presented with the last searched city forecast
+
+
+//     ## Languages Used
+//     - HTML 
+//     - CSS 
+//     - JavaScript 
+//     - jQuery
+//     - Ajax
+
+//     ## Screenshots
+//     How the app should look when loaded --->
+
+//     ![image](assets/images/screenshot.png)`;
+
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
