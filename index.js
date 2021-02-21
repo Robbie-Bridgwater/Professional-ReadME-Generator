733420dc3aebf98832c3d6caaa53b3d893c8ffa9
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-02-21 14:55:28 +0000

    Created the initiliase function, added the appropriate variables, moved the generate readME file to the external js file and figured out how to do this lol

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-// const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
 const questions = [{
-
+    type: 'input',
+    name: 'email',
+    message: 'Email: enter your email.',
+    validate: (answer) => {
+        if (answer.length < 1) {
+            return console.log("You must enter an email.")
+        }
+        return true;
+    },
+}, {
     type: 'input',
     name: 'username',
     message: 'Username: enter your GitHub username.',
-    // validate: (answer) => {
-    //     if (answer.length < 1) {
-    //         return console.log("You must enter a github username.")
-    //     }
-    //     return true;
-    // },
+    validate: (answer) => {
+        if (answer.length < 1) {
+            return console.log("You must enter a github username.")
+        }
+        return true;
+    },
 }, {
     type: 'input',
-    name: 'repository',
-    message: 'Repo name: enter the name of your GitHub repository.',
-    // validate: (answer) => {
-    //     if (answer.length < 1) {
-    //         return console.log("You must a GitHub repository name.")
-    //     }
-    //     return true;
-    // },
+    name: 'liveURL',
+    message: 'Live URL: enter the deployed URL of your GitHub repository.',
+    validate: (answer) => {
+        if (answer.length < 1) {
+            return console.log("You must a Live URL.")
+        }
+        return true;
+    },
 }, {
     type: 'input',
     name: 'title',
     message: 'Title: enter the title of your project.',
-    // validate: (answer) => {
-    //     if (answer.length < 1) {
-    //         return console.log("You must enter a title.")
-    //     }
-    //     return true;
-    // },
+    validate: (answer) => {
+        if (answer.length < 1) {
+            return console.log("You must enter a title.")
+        }
+        return true;
+    },
 }, {
     type: 'input',
     name: 'description',
     message: 'Description: enter a description for your project.',
-    // validate: (answer) => {
-    //     if (answer.length < 1) {
-    //         return console.log("You must enter a description.")
-    //     }
-    //     return true;
-    // },
+    validate: (answer) => {
+        if (answer.length < 1) {
+            return console.log("You must enter a description.")
+        }
+        return true;
+    },
 }, {
     type: 'input',
     name: 'installation',
@@ -54,12 +61,12 @@ const questions = [{
     type: 'input',
     name: 'usage',
     message: 'Usage: enter your project instructions and some examples of it in use.',
-    // validate: (answer) => {
-    //     if (answer.length < 1) {
-    //         return console.log("You must enter usage instructions and/or examples.")
-    //     }
-    //     return true;
-    // },
+    validate: (answer) => {
+        if (answer.length < 1) {
+            return console.log("You must enter usage instructions and/or examples.")
+        }
+        return true;
+    },
 }, {
     type: 'list',
     name: 'license',
@@ -77,56 +84,18 @@ const questions = [{
     //   no validation here, if no tests are desired they can enter nothing and skip this question.
 }, ];
 
-// TODO: Create a function to write README file
-
-// function generateREADME(answers) {
-//     return `# ${questions.title}
-//     - ${questions.description}
-
-//     ## Link
-//     Launch the app ---> [here](${questions.repository})
-
-//     ## User Story
-
-//     - AS A traveler
-//     - I WANT to see the weather outlook for multiple cities
-//     - SO THAT I can plan a trip accordingly
-
-
-//     ## Acceptance Criteria
-//     - GIVEN a weather dashboard with form inputs
-//     - WHEN I search for a city
-//     - THEN I am presented with current and future conditions for that city and that city is added to the search history
-//     - WHEN I view current weather conditions for that city
-//     - THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//     - WHEN I view the UV index
-//     - THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-//     - WHEN I view future weather conditions for that city
-//     - THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-//     - WHEN I click on a city in the search history
-//     - THEN I am again presented with current and future conditions for that city
-//     - WHEN I open the weather dashboard
-//     - THEN I am presented with the last searched city forecast
-
-
-//     ## Languages Used
-//     - HTML 
-//     - CSS 
-//     - JavaScript 
-//     - jQuery
-//     - Ajax
-
-//     ## Screenshots
-//     How the app should look when loaded --->
-
-//     ![image](assets/images/screenshot.png)`;
-
-// }
-
-// TODO: Create a function to initialize app
 const init = () => {
-    inquirer.prompt(questions);
-}
+    inquirer.prompt(questions)
+        .then((response) => {
+            const readme = generateREADME(response);
+            fs.writeFileSync('README.md', generateMarkdown(response));
+        })
+        .then(() => {
+            console.log("README has been successfully generated.")
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+};
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,10 +11,37 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = (answers) => {
+    return `# ${answers.title}
 
-`;
+  ## Link
+  Launch the app ---> [here](${answers.liveURL})
+
+  #### Table of Contents
+  1. [Project Description](#project-description)
+  2. [Installation Instructions](#installation-instructions)
+  3. [Usage Information](#usage-information)
+  4. [Contributor Guidelines](#contributor-guidelines)
+  5. [Test Instructions](#test-instructions)
+  6. [License](#license)
+  7. [Questions](#questions)
+
+  ## Project Description
+  * ${answers.description}
+  ## Installation Instructions
+  * ${answers.install}
+  ## Usage Information
+  * ${answers.usage}
+  ## Contributor Guidelines
+  * ${answers.contributing}
+  ## Test Instructions
+  * ${answers.tests}
+  ## License
+  * licensed under the ${answers.license}
+  ## Questions
+  * For assistance or questions, contact me at ${answers.email}
+  * Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
+
 }
 
 module.exports = generateMarkdown;
