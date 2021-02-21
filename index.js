a9a665235da9b26dd80eabaea15ce6890de58c0a
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-02-21 11:43:37 +0000

    adjusted consts at the top, made them incorrectly initially

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
 const questions = [{
