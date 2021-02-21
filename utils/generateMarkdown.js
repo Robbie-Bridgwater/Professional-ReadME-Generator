a3e071e03c3c8453ba3ade943666716ae3a5264c
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-02-21 15:17:44 +0000

    made the license badge function using a switchboard

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license) {
+        switch (license) {
+            case 'MIT License':
+                return '[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)';
+            case 'GPLv3 License':
+                return '[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)';
+            case 'Mozilla Public License 2.0':
+                return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+            default:
+                "";
+                break;
+        }
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    switch (license) {
+        case 'MIT License':
+            return '\n\n> This project was created under the standard MIT licence.\n\n[Learn more about this licence.](https://lbesson.mit-license.org/)';
+        case 'GPLv3 License':
+            return '\n\n> This project was created under the GNU General Public License.\n\n[Learn more about this licence.](http://www.gnu.org/licenses/gpl-3.0.en.html)';
+        case 'Mozilla Public License 2.0':
+            return '\n\n> This project was created under the Mozilla Public License, Version 2.0.\n\n[Learn more about this licence.](https://www.mozilla.org/en-US/MPL/2.0/)';
+        default:
+            "";
+            break;
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === "Other") {
+        "";
+    } else {
+        let chosenLicense = renderLicenseBadge(license) + renderLicenseLink(license);
+        return chosenLicense;
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (answers) => {
@@ -37,7 +70,7 @@ const generateMarkdown = (answers) => {
   ## Test Instructions
   * ${answers.tests}
   ## License
-  * licensed under the ${answers.license}
+  * licensed under the ${renderLicenseSection(answers.license)}
   ## Questions
   * For assistance or questions, contact me at ${answers.email}
   * Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
